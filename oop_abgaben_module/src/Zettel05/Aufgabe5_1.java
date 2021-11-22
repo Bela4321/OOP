ddc6453c2be5dd702ae8c4d387da55bf14be81b2
Bela <bela.schinke@gmail.com>
2021-11-22 14:33:56 +0100

    Aufgabe5_1

--- oop_abgaben_module/src/Zettel05/Aufgabe5_1.java
+++ oop_abgaben_module/src/Zettel05/Aufgabe5_1.java
@@ -14,8 +14,50 @@ public class Aufgabe5_1 {
             return getMinimum(restOfNumbers);
         }
     }
+
+    static float getMaximun(float[] numbers) {
+        float maximum = numbers[0];
+        for (float number:numbers) {
+            if (number> maximum) {
+                maximum= number;
+            }
+        }
+        return maximum;
+    }
+
+    static float calculateAverage(float[] numbers) {
+        float sum = 0;
+        int i = 0;
+        while (i<numbers.length) {
+            sum += numbers[i];
+            i++;
+        }
+        return (sum/numbers.length);
+    }
+
+    static boolean isSorted(float[] numbers, boolean ascending) {
+        if (ascending) {
+            for (int i = 0;i<numbers.length-1;i++) {
+                if (numbers[i]>numbers[i+1]) {
+                    return false;
+                }
+            }
+            return true;
+        } else {
+            for (int i = 0;i<numbers.length-1;i++) {
+                if (numbers[i]<numbers[i+1]) {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+
     public static void main(String[] args) {
-        float[] test = {1.4f,3.6f,4.3f,.9f,3.9f};
-        getMinimum(test);
+        float[] test = {1.4f,3.6f,4.3f,4.9f};
+        System.out.println(getMinimum(test));
+        System.out.println(getMaximun(test));
+        System.out.println(calculateAverage(test));
+        System.out.println(isSorted(test,true));
     }
 }
