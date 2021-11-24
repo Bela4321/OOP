a09538d376f02709abba50b7577a7eb3dedba6d5
michelo200 <michel.poesze@gmail.com>
2021-11-24 16:33:24 +0100

    Test, Franzi you loose

---
+++ oop_abgaben_module/src/Zettel06/Aufgabe6_1.java
@@ -0,0 +1,14 @@
+package Zettel06;
+
+public class Aufgabe6_1 {
+    static boolean test() {
+        return true;
+    }
+    public static void main(String[] args) {
+        int i = 3;
+        System.out.println(i);
+        if (test()) {
+            System.out.println("Franzi, ich war vor dir hier. Liebste Grüße, dein Lieblings-Michel :-))))");
+        }
+    }
+}
