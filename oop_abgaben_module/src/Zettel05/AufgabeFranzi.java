49cea313cabb2994d5686deade255ce82c3e3d9b
franzi2112 <franziskasitek@gmail.com>
2021-11-25 09:20:16 +0100

    Meine Aufgaben zu Zettel 5

--- oop_abgaben_module/src/Zettel05/AufgabeFranzi.java
+++ oop_abgaben_module/src/Zettel05/AufgabeFranzi.java
@@ -80,7 +80,79 @@ public class AufgabeFranzi {
         return result;
     }
 
+    //2_a)
+
+import java.util.Arrays;static double distance (double [] gps) {
+        int i = 0;
+        double dist = 0;
+        while (i < gps.length - 5){
+            dist = dist + Math.sqrt(Math.pow((gps[i]-gps[i+3]), 2) + Math.pow((gps[i+1]-gps[i+4]), 2) + Math.pow((gps[i+2]-gps[i+5]), 2));
+            i++;
+        }
+        return dist;
+    }
+
+
+
+// 2_b)
+
+    static double velocity(double [] gps){
+        double dist = distance(gps);
+        double time = gps.length / 3;
+        return dist / time;
+    }
+
+
+// 2_c)
+
+    static double maxVelocity(double [] gps) {
+        int i = 0;
+        double maxSpeed = 0;
+        while (i < gps.length - 5) {
+            double [] singleGPS = {gps[i], gps[i+1], gps[i+2], gps[i+3], gps[i+4], gps[i+5]};
+            if (maxSpeed < velocity(singleGPS)) {
+                maxSpeed = velocity(singleGPS);
+            }
+            i = i + 3;
+        }
+        return maxSpeed;
+    }
+
+    //2_d) , funktioniert irgendwie nicht
+    static double [] patrialGPS (double [] gps, double [] start, double [] end) {
+        //double [] startGPS = [ ];
+        //double [] endGPS = [ ];
+        double[] newGPS = [ ];
+        if ((start[0] == 0 && start[1] == 0 && start[2] == 0) || (end[0] == 0 && end[1] == 0 && end[2] == 0)) {
+            return gps;
+        }
+        if ((start[0] < end[0] && start[1] < end[1] && start[2] < end[2])) {
+            return gps;
+        }
+        if (start.length == 3 && end.length == 3) {
+            for (int i = 0; i < 3; i++) {
+                //startGPS [i] = start [i];
+                gps[i] = start[i];
+                newGPS = gps;
+            }
+            //for (int i = 0; i < 3; i++){
+            //endGPS[i] = end [i];
+            newGPS[newGPS.length - 1] = end[2];
+            newGPS[newGPS.length - 2] = end[1];
+            newGPS[newGPS.length - 3] = end[0];
+        }
+        return newGPS;
+
+    }
+
+
+
     public static void main(String[] args){
+
         testAll();
+        double [] a = {-20.0, 0.0, 200.0, -18.5,-0.647, 200.577, -16.85, -1.237, 201.16};
+        distance(a);
+        velocity(a);
+        maxVelocity(a);
     }
 }
