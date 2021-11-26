3f2454d5021bd5040ab805b1660150e18ffa6c3c
michelo200 <michel.poesze@gmail.com>
2021-11-26 13:16:03 +0100

    Test, Franzi you loose

--- oop_abgaben_module/src/Zettel05/michel052.java
+++ oop_abgaben_module/src/Zettel05/michel052.java
@@ -9,13 +9,13 @@ public class michel052 {
     double[] gps; //= {x1,y1,z1,x2,y2,z2,...};
 
     double distance(double[] gps) {
-        double dis = Math.sqrt(Math.pow(gps[gps.length - 2] - gps[0],2) + Math.pow(gps[(gps.length) - 1] - gps[1],2) + Math.pow(gps[gps.length] - gps[2],2));
-    return dis;
+        double dis = Math.sqrt(Math.pow(gps[gps.length - 2] - gps[0], 2) + Math.pow(gps[(gps.length) - 1] - gps[1], 2) + Math.pow(gps[gps.length] - gps[2], 2));
+        return dis;
     }
 
-    double velocity (double[] gps) {
-        double velo = Math.sqrt(Math.pow(gps[gps.length - 2] - gps[0],2) + Math.pow(gps[(gps.length) - 1] - gps[1],2) + Math.pow(gps[gps.length] - gps[2],2))/ gps.length;
-    return velo;
+    double velocity(double[] gps) {
+        double velo = Math.sqrt(Math.pow(gps[gps.length - 2] - gps[0], 2) + Math.pow(gps[(gps.length) - 1] - gps[1], 2) + Math.pow(gps[gps.length] - gps[2], 2)) / gps.length;
+        return velo;
     }
 
     /*
@@ -23,18 +23,17 @@ public class michel052 {
     Dann wird in der for-Schleife die Geschwindigkeit zwischen jeder gelaufenen Sekunde angeschaut. Einheit ist dann schon m/s.
     Wenn diese größer max, dann ist das die neue maxVelocity.
      */
-    double maxVelocity (double[] gps) {
+    double maxVelocity(double[] gps) {
+        double max = 0;
         for (int i = 0; i < gps.length; i = i + 3) {
-            double max = Math.sqrt(Math.pow(gps[gps.length - 2] - gps[0],2) + Math.pow(gps[(gps.length) - 1] - gps[1],2) + Math.pow(gps[gps.length] - gps[2],2)) / gps.length;
-            double newMax = Math.sqrt(Math.pow(gps[i + 3] - gps[i],2) + Math.pow(gps[i + 4] - gps[i + 1],2) + Math.pow(gps[i + 5] - gps[i + 2],2));
+            max = Math.sqrt(Math.pow(gps[gps.length - 2] - gps[0], 2) + Math.pow(gps[(gps.length) - 1] - gps[1], 2) + Math.pow(gps[gps.length] - gps[2], 2)) / gps.length;
+            double newMax = Math.sqrt(Math.pow(gps[i + 3] - gps[i], 2) + Math.pow(gps[i + 4] - gps[i + 1], 2) + Math.pow(gps[i + 5] - gps[i + 2], 2));
             if (newMax > max) {
                 max = newMax;
             }
-            return max;
         }
-    }
-
-    double[] partialGPS(double[] gps, double[] start, double[] end) {
-
+        return max;
     }
 }
+// hier fehlt noch die letzte 5.2.
+
