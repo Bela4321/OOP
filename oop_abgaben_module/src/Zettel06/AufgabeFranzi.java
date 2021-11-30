e2438247ad0df182aa26e28d54daa3e128473645
franzi2112 <franziskasitek@gmail.com>
2021-11-30 17:39:36 +0100

    Meine Überlegungen zu Zettel 6

--- oop_abgaben_module/src/Zettel06/AufgabeFranzi.java
+++ oop_abgaben_module/src/Zettel06/AufgabeFranzi.java
@@ -28,12 +28,28 @@ public class AufgabeFranzi {
 
     //6.2 a)
 
-    int[] primesUpTo (int n) {
-        if (n < 0){
+    static int[] primesUpTo (int n) {
+        if (n < 0) {
             return null;
         }
-        int [] primes = new int[n-1];
+        int[] spaceForAll = new int[n - 1];
         for (int i = 2; i < n; i++) {
+            for (int elem : spaceForAll) {
+                if (i % elem != 0) {
+                    spaceForAll[elem + 1] = i;
+                }
+            }
+        }
+        int counter = 0;
+        for (int zeroes : spaceForAll) {
+            if (zeroes == 0) {
+                counter++;
+            }
+        }
+        int[] primes = Arrays.copyOfRange(spaceForAll, 0, spaceForAll.length - counter);
+        return primes;
+    }
+            /*
             for (int b = 2; b < Math.sqrt(n) + 2; b++) {
                 if (i % b == 0) {
                     primes[i] = 0;
@@ -52,9 +68,26 @@ public class AufgabeFranzi {
         int[] finalPrimes = new int[counter];
         finalPrimes = Arrays.copyOfRange(primes, 0, counter - 1 );
         return finalPrimes;
+                     */
+
+
+    static void testOnehundret () {
+        int[] primes = {2,
+                3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59,
+                61, 67, 71, 73, 79, 83, 89, 97};
+        int[] test = primesUpTo(100);
+        for (int i=0;i<primes.length;i++) {
+            if (primes[i] != test[i]) {
+                System.out.println("test failed");
+            } else {
+                System.out.println("+");
+            }
+        }
     }
+
 
 
     public static void main(String[] args){
         test();
+        testOnehundret();
 }
