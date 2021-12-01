94ce5aa41575ea830e6f81196d44c964bed4b117
Bela4321 <93768776+Bela4321@users.noreply.github.com>
2021-12-01 16:36:14 +0100

    corrected michels 6_2

--- oop_abgaben_module/src/Zettel06/Aufgabe62Michel.java
+++ oop_abgaben_module/src/Zettel06/Aufgabe62Michel.java
@@ -1,38 +1,53 @@
 package Zettel06;
 
+import java.awt.*;
 import java.util.Arrays;
 
 public class Aufgabe62Michel {
 
     static int[] primesUpTo(int n) {
-        int[] arrPrimeZ = new int[n - 2];
+        int[] arrPrimeZ = new int[n - 1];
         if (n < 0) {
             System.out.println("only positiv numbers allowed for this methode");
             return null;
-//array erstellen, der mit den Elementen 2 bis n gefüllt ist.
-            for (int i = 2; i <= n; i++) {
-                arrPrimeZ[i - 2] = i;
-            }
-            for (int i1 = arrPrimeZ[0] * 2; i1 % arrPrimeZ[0] == 0 && i1 <= (Math.sqrt(n) + 2); i1++)
-                arrPrimeZ[i1] = 0;
         }
+//array erstellen, der mit den Elementen 2 bis n gefüllt ist.
+        for (int i = 2; i <= n; i++) {
+            arrPrimeZ[i - 2] = i;
+        }
+
+        for (int i1 = 2; i1 <= (Math.sqrt(n) + 2); i1++) {
+            for (int i2 = 0; i2 < arrPrimeZ.length; i2++) {
+                if (arrPrimeZ[i2] % i1 == 0 && arrPrimeZ[i2] != i1) {
+                    arrPrimeZ[i2] =0;
+                }
+            }
+        }
+
+
         int countPrime = 0;
+
         for (int j : arrPrimeZ) {
             if (j != 0) {
                 countPrime++;
             }
-            int[] arrPrime = new int[countPrime];
-            int countPrime = 0;
-            for (int j1 : arrPrimeZ) {
-                if (j1 != 0) {
-                    arrPrime[countPrime] = j1;
-                    countPrime++;
-                }
+        }
+        int[] arrPrime = new int[countPrime];
+        countPrime = 0;
+        for (int j1 : arrPrimeZ) {
+            if (j1 != 0) {
+                arrPrime[countPrime] = j1;
+                countPrime++;
             }
-            return arrPrime;
         }
 
-        public static void main (String[]args){
-        }
+        return arrPrime;
     }
-}
+
+    public static void main(String[] args) {
+        System.out.println(primesUpTo(100).length);
+        for (int i: primesUpTo(100)) {
+            System.out.println(i);
+
+    }
+}}
