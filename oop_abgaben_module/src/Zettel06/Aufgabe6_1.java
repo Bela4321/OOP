f32b9f5c4f3ff347510a7bf29a698ed27bdebcc9
Bela4321 <93768776+Bela4321@users.noreply.github.com>
2021-12-01 16:44:13 +0100

    testin merge confilcts

--- oop_abgaben_module/src/Zettel06/Aufgabe6_1.java
+++ oop_abgaben_module/src/Zettel06/Aufgabe6_1.java
@@ -10,8 +10,10 @@ public class Aufgabe6_1 {
    public static void main(String[] args) {
         double r1=60;
         double r2=40;
+        //hallo Michel
         double r3=50;
         double r4=50;
+        //moin moin
         double r5=70;
         double r6=80;
         System.out.println(parallelCircuit(seriesCircuit(parallelCircuit(r1,r2),r3),parallelCircuit(seriesCircuit(r4,r5),r6)));
