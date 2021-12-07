0581d3152a4cea274de1eab7a481a22ab1215981
Bela <bela.schinke@gmail.com>
2021-12-07 12:52:56 +0100

    Added partial test for 7.2

--- oop_abgaben_module/src/Zettel07/Library.java
+++ oop_abgaben_module/src/Zettel07/Library.java
@@ -57,5 +57,12 @@ public class Library {
         myDVDCollection.addDVD(new DVD("Never gonna give you up","1234",new Director("Rick","Astley",new Date(1966,1,6)), actors1));
         Actor[] actors2 = {new Actor("Dinosour","one",new Date(-200000000,2,12)),new Actor("Dinosour","two",new Date(-200000000,5,15))};
         myDVDCollection.addDVD(new DVD("Jurassic Park","420",new Director("Michael", "Jordan", new Date(1820,1,31)),actors2));
+        Actor[] actors3 = new Actor[0];
+        myDVDCollection.addDVD(new DVD("Seven Minutes of Silence","69420",new Director("Stephen", "Hawking",new Date(0,0,0)),actors3));
+        Actor[] actors4 = {new Actor("Bela", "Schinke",new Date(2100)), new Actor("Michel","Poesze",new Date(1969)), new Actor("Franzi", "Sitek",new Date(2004,1,29))};
+        myDVDCollection.addDVD(new DVD("Der neue OOP Zettel -- Ein Drama in drei Akten -- Teil 1","314159", new Director("OOP Zettel 7","Aufgabe 1",new Date(2021,11,6)),actors4));
+        myDVDCollection.addDVD(new DVD("Der neue OOP Zettel -- Ein Drama in drei Akten -- Teil 2","951413", new Director("OOP Zettel 7","Aufgabe 2",new Date(2021,11,6)),actors4));
+        myDVDCollection.addDVD(new DVD("Der neue OOP Zettel -- Ein Drama in drei Akten -- Teil 3","314159", new Director("OOP Zettel 7","Aufgabe 3",new Date(2021,11,6)),actors4));
+
     }
 }
