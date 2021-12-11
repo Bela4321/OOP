c26a24210a30b0229ed22852bfc327d5cb944d64
Bela <bela.schinke@gmail.com>
2021-12-11 18:38:24 +0100

    Comments for michel

--- oop_abgaben_module/src/Zettel07michel/Position.java
+++ oop_abgaben_module/src/Zettel07michel/Position.java
@@ -1,9 +1,9 @@
 package Zettel07michel;
 
 public class Position {
-    public final char[][] positionChar = new char[5][5];
+    public final char[][] positionChar = new char[5][5];//no, one int for x and one int for y coordinate. no array.
 
-    //private Position findInSquare (char c) {
+    //private Position findInSquare (char c) {//only constuctor is needed in this class, other function goes in Playfair
 
     //}
 }
