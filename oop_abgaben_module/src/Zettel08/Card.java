12249b38ee44ed9c2d3a978cd220b4f2d4c91646
Bela4321 <93768776+Bela4321@users.noreply.github.com>
2021-12-15 20:39:25 +0100

    Aufgabe 8.2 finished

--- oop_abgaben_module/src/Zettel08/Card.java
+++ oop_abgaben_module/src/Zettel08/Card.java
@@ -3,20 +3,21 @@ package Zettel08;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.Date;
-import java.util.Scanner;
 
 interface Comparable{
-    Card[] sort(Card[] cards);
+    static Card[] sort(Card[] cards) {
+        return new Card[0];
+    }
 }
 
 public class Card implements Comparable {
     public final String NAME;
-    public final Date RELEASE_DATE;
+    public final int RELEASE_YEAR;
     private final int TYPE;
 
-    public Card(String NAME,Date RELEASE_DATE,String TYPE) {
+    public Card(String NAME, int RELEASE_YEAR, String TYPE) {
         this.NAME =NAME;
-        this.RELEASE_DATE=RELEASE_DATE;
+        this.RELEASE_YEAR = RELEASE_YEAR;
         if (TYPE.equals("TRAP")){
             this.TYPE = 3;
         } else if (TYPE.equals("SPELL")) {
@@ -44,12 +45,11 @@ public class Card implements Comparable {
         return "ERROR: Invalid Type";
     }
 
-    public Card[] sort(Card[] cards) {
+    public static Card[] sort(Card[] cards) {
         Arrays.sort(cards, new Comparator<Card>() {
-            @Override
             public int compare(Card o1, Card o2) {
-                if (o1.RELEASE_DATE.getYear()!=o2.RELEASE_DATE.getYear()) {
-                    return o1.RELEASE_DATE.getYear()-o2.RELEASE_DATE.getYear();
+                if (o1.RELEASE_YEAR!=o2.RELEASE_YEAR) {
+                    return o1.RELEASE_YEAR-o2.RELEASE_YEAR;
                 } else if (o1.TYPE!= o2.TYPE) {
                     return o1.TYPE-o2.TYPE;
                 } else {
@@ -60,5 +60,31 @@ public class Card implements Comparable {
         return cards;
     }
 
+    public String toString() {
+        return "Card{" +
+                "NAME='" + NAME + '\'' +
+                ", RELEASE_YEAR=" + RELEASE_YEAR +
+                ", TYPE=" + this.getType() +
+                '}';
+    }
+
+    public static void main(String[] args) {
+        Card card1 = new Card("Gandalf", 1959,"SPELL");
+        Card card2 = new Card("Bilbo",1259,"TRAP");
+        Card card3 = new Card("Bela",1259,"EFFECTMONSTER");
+        Card card4 = new Card("Franzi",1042,"EFFECTMONSTER");
+        Card card5 = new Card("Michel",1259,"SPELL");
+        Card card6 = new Card("Gandalf",1259,"SPELL");
+        Card card7 = new Card("Peter",2015,"MONSTER");
+        Card[] cardsArray = {card1, card2,card3,card4,card5,card6,card7,card3,card4,card7};
+        for (Card card:cardsArray) {
+            System.out.println(card);
+        }
+        cardsArray= Card.sort(cardsArray);
+        System.out.println();
+        for (Card card:cardsArray) {
+            System.out.println(card);
+        }
+    }
 
 }
