297ed7c9d6192c8287665cad80f53dc313d9950e
franzi2112 <franziskasitek@gmail.com>
2021-12-15 09:08:29 +0100

    Zettel 7 Aufgabe 1

---
+++ oop_abgaben_module/src/Zettel07Franzi_/Position.java
@@ -0,0 +1,13 @@
+package Zettel07Franzi_;
+
+public class Position {
+    //e
+        public final int xCoordinate;
+        public final int yCoordinate;
+
+        Position (int x, int y) {
+            xCoordinate = x;
+            yCoordinate = y;
+        }
+    }
+
