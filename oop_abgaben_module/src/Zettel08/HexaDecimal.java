51d5856c84b66c759409c6afa12c052d0ee76913
Bela <bela.schinke@gmail.com>
2021-12-16 10:07:22 +0100

    Added tests for 8.3

--- oop_abgaben_module/src/Zettel08/HexaDecimal.java
+++ oop_abgaben_module/src/Zettel08/HexaDecimal.java
@@ -6,7 +6,7 @@ public class HexaDecimal implements Number{
     private String ValueInHex;
 
     public String toString() {
-        return ValueInHex;
+        return ValueInHex +" (="+toIntValue()+")";
     }
 
     HexaDecimal(String hexzahl) {
@@ -50,9 +50,30 @@ public class HexaDecimal implements Number{
         HexaDecimal num2 = new HexaDecimal("5B2");
         System.out.println("num1: "+num1);
         System.out.println("num2: "+num2);
-        System.out.println(num1.toIntValue());
-        System.out.println(num2.toIntValue());
+        System.out.println("\nnum1 = 100(base10) \nnum2 = 64(base10) ");
         num1.fromIntValue(100);
+        num2.fromIntValue(64);
         System.out.println("num1: "+num1);
+        System.out.println("num2: "+num2);
+        System.out.println("\nnum1 = num1 + num1 \nnum2 = num2 + num1 ");
+        num1.add(num1);
+        num2.add(num1);
+        System.out.println("num1: "+num1);
+        System.out.println("num2: "+num2);
+        System.out.println("\nnum1 = num1 * 10\nnum2 = num2 * num1");
+        num1.multiply(new HexaDecimal("A"));
+        num2.multiply(num1);
+        System.out.println("num1: "+num1);
+        System.out.println("num2: "+num2);
+        System.out.println("\nnum1 = num1 - 150\nnum2 = num2 - num1");
+        num1.substract(new HexaDecimal("96"));
+        num2.substract(num1);
+        System.out.println("num1: "+num1);
+        System.out.println("num2: "+num2);
+        System.out.println("\nnum1 = num1 / 50\nnum2 = num2 / 25");
+        num1.divide(new HexaDecimal("32"));
+        num2.divide(new HexaDecimal("19"));
+        System.out.println("num1: "+num1);
+        System.out.println("num2: "+num2);
     }
 }
