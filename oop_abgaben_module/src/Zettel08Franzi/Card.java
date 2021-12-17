81a4448d6b10ed16c40a74be510d2c3102afe9b8
franzi2112 <franziskasitek@gmail.com>
2021-12-17 14:46:59 +0100

    Zettel 8 Aufgabe 1 und 2 Danke Bela! Michel google!!!!

--- oop_abgaben_module/src/Zettel08Franzi/Card.java
+++ oop_abgaben_module/src/Zettel08Franzi/Card.java
@@ -2,55 +2,67 @@ package Zettel08Franzi;
 
 
 import java.util.Arrays;
+import java.util.Comparator;
 
 public class Card implements Comparable {
-    private String name;
-    private String yearOfPublication;
-    private static final String NORMAL_MONSTER = "Normale Monster";
-    private static final String EFFECT_MONSTER = "Effektmonster";
-    private static final String SPELL = "Zauberkarte";
-    private static final String TRAP = "Fallenkarten";
+    public final String NAME;
+    public final int RELEASE_YEAR;
+    private final int TYPE;
 
-    public String getName(){
-        return name;
-    }
-
-    public String getYearOfPublication(){
-        return yearOfPublication;
-    }
-
-    public static String getNormalMonster() {
-        return NORMAL_MONSTER;
-    }
-
-    public static String getEffectMonster() {
-        return EFFECT_MONSTER;
-    }
-
-    public static String getSPELL() {
-        return SPELL;
-    }
-
-    public static String getTRAP() {
-        return TRAP;
-    }
-
-
-
-    public Arrays sortCards(String [] cards) {
-        int a = cards.length;
-        String [] sortedCards = new String [a];
-        for (int i = 2; i < cards.length - 2; i = i + 3){
-            if (cards[i].equals(cards[i+3])){
-                
-            }
+    public Card(String NAME, int RELEASE_YEAR, String TYPE) {
+        this.NAME = NAME;
+        this.RELEASE_YEAR = RELEASE_YEAR;
+        if (TYPE.equals("Trap")) {
+            this.TYPE = 3;
+        } else if (TYPE.equals("Spell")) {
+            this.TYPE = 2;
+        } else if (TYPE.equals("EFFECTMONSTER")) {
+            this.TYPE = 1;
+        } else if (TYPE.equals("NORMALMONSTER")) {
+            this.TYPE = 0;
+        } else {
+            System.out.println("No Correct Type given");
+            this.TYPE = -1;
         }
-
-        return null;
     }
 
-    @Override
-    public Arrays sortCards() {
-        return null;
+    public String getType() {
+        if (this.TYPE == 3) {
+            return "TRAP";
+        } else if (this.TYPE == 2) {
+            return "SPELL";
+        } else if (this.TYPE == 1) {
+            return "EFFECTMONSTER";
+        } else if (this.TYPE == 0) {
+            return "NORMALMONSTER";
+        }
+        return "ERROR: Invalid Type";
     }
+
+    public static Card[] sort(Card[] cards) {
+        Arrays.sort(cards, new Comparator<Card>() {
+            @Override
+            public int compare(Card o1, Card o2) {
+                if (o1.RELEASE_YEAR != o2.RELEASE_YEAR) {
+                    return o1.RELEASE_YEAR - o2.RELEASE_YEAR;
+                } else if (o1.TYPE != o2.TYPE) {
+                    return o1.TYPE - o2.TYPE;
+                } else {
+                    return o1.NAME.compareToIgnoreCase(o2.NAME);
+                }
+            }
+        });
+
+        return cards;
+
+    }
+    public String toString() {
+        return "Card{" +
+                "NAME='" + NAME + '\'' +
+                ", RELEASE_YEAR=" + RELEASE_YEAR +
+                ", TYPE=" + this.getType() +
+                '}';
+    }
+
+
 }
--- oop_abgaben_module/src/Zettel08Franzi/Comparable.java
+++ oop_abgaben_module/src/Zettel08Franzi/Comparable.java
@@ -3,10 +3,8 @@ package Zettel08Franzi;
 import java.util.Arrays;
 
 public interface Comparable {
-        public Arrays sortCards();
-        /*
-        public String sortedByType();
-        public Arrays sortedByName();
-        */
+        static Card[] sort(Card [] Cards ) {
+                return new Card[0];
+        }
 
 }
