d268741b0ca5dd9487d2451f422ab210a61dad96
Bela <bela.schinke@gmail.com>
2021-12-22 19:32:59 +0100

    more elegant card types

--- oop_abgaben_module/src/Zettel08/Card.java
+++ oop_abgaben_module/src/Zettel08/Card.java
@@ -1,7 +1,6 @@
 package Zettel08;
 
 import java.util.Arrays;
-import java.util.Comparator;
 
 
 interface Comparable{
@@ -13,44 +12,37 @@ interface Comparable{
 public class Card implements Comparable {
     public final String NAME;
     public final int RELEASE_YEAR;
-    private final int TYPE;
+    private final int TYPE_ID;
+    private final String[] TYPE_ID_TO_STRING = {"MONSTER", "EFFECTMONSTER", "SPELL", "TRAP"};
 
     public Card(String NAME, int RELEASE_YEAR, String TYPE) {
         this.NAME =NAME;
         this.RELEASE_YEAR = RELEASE_YEAR;
-        if (TYPE.equals("TRAP")){
-            this.TYPE = 3;
-        } else if (TYPE.equals("SPELL")) {
-            this.TYPE=2;
-        } else if (TYPE.equals("EFFECTMONSTER")) {
-            this.TYPE=1;
-        } else if (TYPE.equals("MONSTER")) {
-            this.TYPE=0;
-        } else {
-            System.out.println("No Correct Type givem");
-            this.TYPE=-1;
+        if (Arrays.asList(TYPE_ID_TO_STRING).contains(TYPE)) {
+            this.TYPE_ID= Arrays.asList(TYPE_ID_TO_STRING).indexOf(TYPE);
+            return;
         }
+        System.out.println("No Correct Type givem");
+        this.TYPE_ID =-1;
+
     }
 
     public String getType() {
-        if (this.TYPE==3){
-            return "TRAP";
-        } else if (this.TYPE==2) {
-            return "SPELL";
-        } else if (this.TYPE==1) {
-            return "EFFECTMONSTER";
-        } else if (this.TYPE==0) {
-            return "MONSTER";
+        try {
+            return TYPE_ID_TO_STRING[TYPE_ID];
+        } catch (Exception e) {
+            System.out.println("Invalid Cardtype--check initialisation");
+            return "";
         }
-        return "ERROR: Invalid Type";
+
     }
 
     public static Card[] sort(Card[] cards) {
         Arrays.sort(cards, (o1, o2) -> {
             if (o1.RELEASE_YEAR!=o2.RELEASE_YEAR) {
                 return o1.RELEASE_YEAR-o2.RELEASE_YEAR;
-            } else if (o1.TYPE!= o2.TYPE) {
-                return o1.TYPE-o2.TYPE;
+            } else if (o1.TYPE_ID != o2.TYPE_ID) {
+                return o1.TYPE_ID -o2.TYPE_ID;
             } else {
                 return o1.NAME.compareToIgnoreCase(o2.NAME);
             }
