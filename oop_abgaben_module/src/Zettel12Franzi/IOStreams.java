616c582ab42335488088013bdd97b8494a18a02c
franzi2112 <franziskasitek@gmail.com>
2022-02-10 23:56:54 +0100

    Tests fehlen noch und Aufgabe 2 Könnt man mal bei den Tests gucken bekomme es nicht hin

---
+++ oop_abgaben_module/src/Zettel12Franzi/IOStreams.java
@@ -0,0 +1,15 @@
+/*package Zettel12Franzi;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.stream.Stream;
+
+public class IOStreams {
+    public static Stream<String> words (String filePath) throws FileNotFoundException {
+        BufferedReader bufferedReader = new BufferedReader(new FileReader(filePath));
+        Stream <String> wordStream = bufferedReader.lines().
+
+    }
+}
+*/
