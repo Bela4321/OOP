a3e7736443cb12e8db485da6a37aa7e76154f032
franzi2112 <franziskasitek@gmail.com>
2022-02-11 21:27:11 +0100

    Tests fehlen noch und Aufgabe 2 Könnt man mal bei den Tests gucken bekomme es nicht hin

--- oop_abgaben_module/src/Zettel12Franzi/IOStreams.java
+++ oop_abgaben_module/src/Zettel12Franzi/IOStreams.java
@@ -1,15 +1,44 @@
-/*package Zettel12Franzi;
+package Zettel12Franzi;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
+import java.io.*;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.stream.Stream;
 
 public class IOStreams {
-    public static Stream<String> words (String filePath) throws FileNotFoundException {
+    public static Stream <String> words (String filePath) throws FileNotFoundException {
         BufferedReader bufferedReader = new BufferedReader(new FileReader(filePath));
-        Stream <String> wordStream = bufferedReader.lines().
+        Stream <String> wordStream = bufferedReader.lines();
+        return wordStream;
+    }
+
+    public static Map<String, Integer> countWords(Stream<String> words){
+        HashMap<String, Integer> wordCount = new HashMap<>();
+        words.map(x -> x.split(" ")).flatMap(x -> Arrays.stream(x)).forEach(x -> { if(wordCount.containsKey(x) == true){
+                                wordCount.put(x, wordCount.get(x) + 1);
+                            } else { wordCount.put(x, 1);
+                            }
+                            });
+        return wordCount;
+
+    }
+    public static void writeCountedWords(Map<String, Integer> countedWords, String filepath) throws FileNotFoundException {
+        PrintWriter result = new PrintWriter(filepath);
+        countedWords.forEach((str,in) -> result.println(String.format("%s : %d",str,in)));
+        result.close();
+    }
+
+
+
+
+
+    public static void main(String[] args) throws FileNotFoundException {
+        Stream<String> wordsStream = words("oop_abgaben_module\\src\\Zettel12Franzi\\test.txt");
+        Map<String, Integer> countWords = countWords(wordsStream);
+        System.out.println( countWords.entrySet());
 
     }
 }
-*/
+
+
