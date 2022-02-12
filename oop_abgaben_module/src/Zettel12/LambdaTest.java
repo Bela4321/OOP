c622700e3edce18faa3837cd06d84e887f279b77
Bela <bela.schinke@gmail.com>
2022-02-12 13:14:32 +0100

    LambdaTest with 10 elements

--- oop_abgaben_module/src/Zettel12/LambdaTest.java
+++ oop_abgaben_module/src/Zettel12/LambdaTest.java
@@ -13,13 +13,13 @@ class LambdaTest {
 
     @Test
     void removeIf() {
-        ArrayList<String> testlist1 = new ArrayList<>(Arrays.asList("moin","servus", "Moin","this is a stream","yes?","!"));
-        ArrayList<Integer> testlist2 = new ArrayList<>(Arrays.asList(-1,1,2135,-32,23,0,-12));
+        ArrayList<String> testlist1 = new ArrayList<>(Arrays.asList("moin","servus", "Moin","this", "is", "a", "stream","yes?","!"));
+        ArrayList<Integer> testlist2 = new ArrayList<>(Arrays.asList(-1,-1,1,2135,-32,23,32,0,0,-12));
         Predicate<String> stringPredicate = x -> x.length() <= 4;
         Predicate<Integer> integerPredicate = x -> x<=0;
 
-        List<String> expectedlist1 = Arrays.asList("servus","this is a stream");
-        List<Integer> expectedlist2 = Arrays.asList(1,2135,23);
+        List<String> expectedlist1 = Arrays.asList("servus", "stream");
+        List<Integer> expectedlist2 = Arrays.asList(1,2135,23,32);
 
         Lambda.removeIf(testlist1,stringPredicate);
         Lambda.removeIf(testlist2, integerPredicate);
@@ -30,13 +30,13 @@ class LambdaTest {
 
     @Test
     void sortBy() {
-        ArrayList<String> testlist1 = new ArrayList<>(Arrays.asList("moin","servus", "Moin","this is a stream","yes?","!"));
-        ArrayList<Integer> testlist2 = new ArrayList<>(Arrays.asList(-1,1,2135,-32,23,0,-12));
+        ArrayList<String> testlist1 = new ArrayList<>(Arrays.asList("moin","servus", "Moin","this", "is", "a", "stream","yes?","!"));
+        ArrayList<Integer> testlist2 = new ArrayList<>(Arrays.asList(-1,-1,1,2135,-32,23,32,0,0,-12));
         Comparator<String> stringComp = (x,y) -> (x.length()-y.length());
         Comparator<Integer> intComp = (x,y) -> Math.abs(y)%10 -Math.abs(x)%10;
 
-        List<String> expectedstrlist = Arrays.asList("!","moin", "Moin","yes?","servus","this is a stream");
-        List<Integer> expectedintlist = Arrays.asList(2135,23,-32,-12,-1,1,0);
+        List<String> expectedstrlist = Arrays.asList("a", "!", "is","moin", "Moin","this","yes?","servus", "stream");
+        List<Integer> expectedintlist = Arrays.asList(2135,23,-32,32,-12,-1,-1,1,0,0);
 
         Lambda.sortBy(testlist1,stringComp);
         Lambda.sortBy(testlist2,intComp);
@@ -47,10 +47,10 @@ class LambdaTest {
 
     @Test
     void listToString() {
-        ArrayList<String> testlist1 = new ArrayList<>(Arrays.asList("moin","servus", "Moin","this is a stream","yes?","!"));
-        ArrayList<Integer> testlist2 = new ArrayList<>(Arrays.asList(-1,1,2135,-32,23,0,-12));
-        String expected1 = "moin\nservus\nMoin\nthis is a stream\nyes?\n!\n";
-        String expected2 = "-1\n1\n2135\n-32\n23\n0\n-12\n";
+        ArrayList<String> testlist1 = new ArrayList<>(Arrays.asList("moin","servus", "Moin","this", "is", "a", "stream","yes?","!"));
+        ArrayList<Integer> testlist2 = new ArrayList<>(Arrays.asList(-1,-1,1,2135,-32,23,32,0,0,-12));
+        String expected1 = "moin\nservus\nMoin\nthis\nis\na\nstream\nyes?\n!\n";
+        String expected2 = "-1\n-1\n1\n2135\n-32\n23\n32\n0\n0\n-12\n";
 
         assertEquals(expected1,Lambda.listToString(testlist1));
         assertEquals(expected2, Lambda.listToString(testlist2));
