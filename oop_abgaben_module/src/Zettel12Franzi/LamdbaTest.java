5f8b1bdcfe50a93d7a33ff844137c9d1329b201d
Bela <bela.schinke@gmail.com>
2022-02-12 14:00:21 +0100

    Added @Test + void test method

--- oop_abgaben_module/src/Zettel12Franzi/LamdbaTest.java
+++ oop_abgaben_module/src/Zettel12Franzi/LamdbaTest.java
@@ -1,4 +1,6 @@
-/*package Zettel12Franzi;
+package Zettel12Franzi;
+
+import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 import java.util.Comparator;
@@ -9,7 +11,8 @@ import java.util.function.Predicate;
 import static org.junit.jupiter.api.Assertions.*;
 
 class LamdbaTest {
-
+    @Test
+    void test() {
     ArrayList <Integer> list1 = new ArrayList<>(List.of(1, 33, 22, 57, 5, 8, 13, 68, 45, 10 ));
         ArrayList <String> list2 = new ArrayList<>(List.of("Hi", "Wie", "Geht", "es", "euch", "so", "das ist ein" , "cooler", "test", "yeaahh" ));
         ArrayList <Objects> list3 = new ArrayList(List.of(1, "Hi", 'c', 57, "gut", 8, "und", 'd', "so", 10 ));
@@ -19,16 +22,16 @@ class LamdbaTest {
         Comparator <Integer> comparator1 = (c,d) -> Integer.valueOf(c) - Integer.valueOf(d);
         Comparator <String> comparator2 = (e, f) -> e.length();
         Comparator <Objects> comparator3 = (g,h) -> g.hashCode();
-        removeIf(list1,filter1);
-        removeIf(list2,filter2);
-        removeIf(list3, filter3);
-        sortBy(list1,comparator1);
-        sortBy(list2, comparator2);
-        sortBy(list3, comparator3);
-        listToString(list1);
-        listToString(list2);
-        listToString(list3);
-
+        Lamdba.removeIf(list1, filter1);
+        Lamdba.removeIf(list2,filter2);
+        Lamdba.removeIf(list3, filter3);
+        Lamdba.sortBy(list1,comparator1);
+        Lamdba.sortBy(list2, comparator2);
+        Lamdba.sortBy(list3, comparator3);
+        Lamdba.listToString(list1);
+        Lamdba.listToString(list2);
+        Lamdba.listToString(list3);
+    }
 
 //weiß nicht wie diese Comparator funktionieren und es hat irgendwie nicht wirklich funktioniert, weiß nicht genau ob das daran liegt
 // wie ich die Test Objekte geschrieben habe oder an meinen Klassen.
@@ -39,5 +42,3 @@ class LamdbaTest {
 
 
 }
-
- */
