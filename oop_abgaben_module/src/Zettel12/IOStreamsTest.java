d3173d84cb29e535e3df7b8c1e77d825f7db4d24
franzi2112 <franziskasitek@gmail.com>
2022-02-14 11:17:25 +0100

    Nur Test bei der 2 noch nicht

--- oop_abgaben_module/src/Zettel12/IOStreamsTest.java
+++ oop_abgaben_module/src/Zettel12/IOStreamsTest.java
@@ -12,7 +12,7 @@ import java.util.stream.Collectors;
 import static org.junit.jupiter.api.Assertions.*;
 
 class IOStreamsTest {
-    //compare the two text files, needs to ececute IOStreams.main() first
+    //compare the two text files, needs to execute IOStreams.main() first
 
     @Test
     void filecompartor() throws FileNotFoundException {
